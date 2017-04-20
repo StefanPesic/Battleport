fdb0518a5db44cd33622faf21a7311a87b2e0977
Stefan Pesic <sp.pesic@live.nl>
2017-04-20 11:04:58 +0200

    Added functionality's in a class
    And the game is tested and it runs

--- Game/TheMenu_remastered.py
+++ Game/TheMenu_remastered.py
@@ -2,6 +2,48 @@ from functions_remastered import *
 import pygame_textInput_remastered
 from pygame_textInput_remastered import *
 import psycopg2
+import sys
+
+#TODO database login
+#Database login: (host= "localhost", database="postgres", user= "postgres", password="WallSpeaker5" )
+
+def menuFunction():
+    fps = Clock(60)
+
+    display = Display("1108x819", 1600,900)
+    display.Update()
+
+    image = ImageLoad("menu.jpg")
+    image.blit(display)
+
+    font = Font(None, 90, "Menutest", white)
+    font.blit(display,350,10)
 
 
+    mainMenu = True
+    while mainMenu == True:
+        for events in pygame.event.get():
+            if events.type == pygame.QUIT:
+                pygame.quit(); sys.exit()
+            elif events.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+            elif events.type == pygame.MOUSEBUTTONUP:
+                 pass
+
+            elif events.type == pygame.MOUSEMOTION:
+                pass
+
+            if events.type == pygame.KEYDOWN:
+                if events.key == pygame.K_ESCAPE:
+                    pygame.quit();
+                    sys.exit()
+
+            # the_menu_click(MouseX, MouseY)
+            (MouseX, MouseY) = pygame.mouse.get_pos()
+
+        pygame.display.update()
+
+
+menuFunction()
 
--- Game/functions_remastered.py
+++ Game/functions_remastered.py
@@ -2,7 +2,6 @@ import pygame
 import psycopg2
 
 ''''Functions remastered'''
-
 pygame.init()
 
 class Colour:
@@ -10,6 +9,9 @@ class Colour:
         self.colourOne = colourOne
         self.colourTwo = colourTwo
         self.colourThree = colourThree
+    def showColour(self):
+        tuple = (self.colourOne, self.colourTwo, self.colourThree)
+        return tuple
 
 white = Colour(255,255,255)
 black = Colour(0,0,0)
@@ -49,22 +51,32 @@ playerhead_title = Title_position(855,1020,0,35)
 normalcard1_title = Title_position(775,935,65,180)
 
 class Display:
-    def __init__(self, width, height):
+    def __init__(self, captiontitle, width, height):
+        self.caption = pygame.display.set_caption(captiontitle)
         self.screen = pygame.display.set_mode((width, height))
         self.fullScreen =  pygame.display.set_mode((width, height), pygame.FULLSCREEN)
     def displayChange(self, click):
-        if click == 0:
-            return self.screen
-        elif click == 1:
-            return pygame.display.set_mode((800,600), pygame.FULLSCREEN)
-        elif click == 2:
-            return self.fullScreen
+        pass
+    def Update(self):
+        return pygame.display.update()
 
-display640x480 = Display(640,480)
-display1108x819 = Display(1108,819)
+class ImageLoad:
+    def __init__(self, imageFile):
+        self.imageFile = imageFile
+        self.imageLoading = pygame.image.load(self.imageFile)
+        pygame.transform.scale(self.imageLoading, (1600,900))
+    def blit(self, screenBox):
+        return screenBox.screen.blit(self.imageLoading, (500, 0))
+
+class Font:
+    def __init__(self, styleFont, sizeFont, text, colour):
+        self.fontSet = pygame.font.Font(styleFont, sizeFont)
+        self.fontRender = self.fontSet.render(str(text), 5, colour.showColour()) #TODO edit
+    def blit(self, screenBox, posX, posY):
+        return screenBox.screen.blit(self.fontRender,(posX, posY))
 
 class Music:
-    def __init__(self, audioFile, volume, typePlay):
+    def __init__(self, volume, audioFile, typePlay):
         self.audiofile = pygame.mixer.music.load(audioFile)
         self.volume = pygame.mixer.music.set_volume(volume)
         self.typePlay = pygame.mixer.music.play(typePlay)
@@ -74,14 +86,37 @@ class Music:
         scroll = 10
         pass
 
-musicPlay = Music(100, "musictheme.wav", -1)
 effectSilencer = Music(100, "silencer.wav", -1)
 effectCannon = Music(100, "cannon.wav", -1)
-effectNotAvailable = Music(100, "not_available.wav", -1)
+musicPlay = Music(100, "musictheme.wav", -1)
 
 class DataBase:
     def __init__(self, host, database, user, password):
         self.connection = psycopg2.connect(host= host, database= database, user= user, password= password)
+        self.connection.autocommit = True
+        self.cur = self.connection.cursor()
 
+class Card: #TODO make 12 instances of card
+    def __init__(self):
+        self.isGrab = False
 
+class specialBoatGrab: #TODO 9 instances of card, with 9 specialboat9grabs
+    def __init__(self):
+        self.isSpecialGrab = True
 
+class Player(ImageLoad):
+    def __init__(self, turn, playerGrab):
+        self.name = ""
+        self.isTurn = turn
+        self.isPlayerGrab = playerGrab
+        self.score = 0
+    def playerWinner(self):
+        for events in pygame.event.get():
+            pass
+            if events.type == pygame.KEYDOWN:
+                if events.key == pygame.K_SPACE:
+                    pass #TODO need to go to the menu
+
+class Boat: #TODO make 9 instances of boat
+    def __init__(self):
+        self.isActiveBoat = False
